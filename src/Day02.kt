13aea522fa1d01cc5c10243184f9ba491586eebb
Felix <felixschmidbb@gmail.com>
2022-12-02 10:26:19 +0100

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
-    }
-
-
-    fun part2(input: List<String>): Int {
-        TODO()
-    }
-
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
+
+fun part1(input: List<String>): Int {
+    return input.map(::parseLine)
+        .sumOf { (opponent, player) ->
+            val baseScore = player + 1
+            val gameScore = when ((player - opponent).mod(3)) {
+                0 -> 3
+                1 -> 6
+                else -> 0
+            }
+            baseScore + gameScore
+        }
+}
+
+
+fun part2(input: List<String>): Int {
+    return input.map(::parseLine)
+        .sumOf { (opponent, player) ->
+            val gameScore = player * 3
+            val move = when (gameScore) {
+                0 -> 2
+                3 -> 0
+                else -> 1
+            }
+            val baseScore = (move + opponent).mod(3) + 1
+            baseScore + gameScore
+        }
+}
+
+fun parseLine(line: String): Pair<Int, Int> = line.first() - 'A' to line.last() - 'X'
