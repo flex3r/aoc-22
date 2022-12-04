3f3d4b32a67fad153940d2298b37bca2cc1c56ee
Felix <felixschmidbb@gmail.com>
2022-12-04 10:15:09 +0100

    Prepare Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val testInput = readInput("Day05_test")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 1)
+
+    val input = readInput("Day05")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    TODO()
+}
+
+private fun part2(input: List<String>): Int {
+    TODO()
+}
